2b8a8b92fa7f419c1d4cabbd80d24e43ea275f33
Izzy-hufty <izzyhufty@gmail.com>
2025-03-09 20:07:33 -0500

    Changed setpoints to easier to read names

--- Konshu-2025/src/main/java/frc/robot/subsystems/SSM.java
+++ Konshu-2025/src/main/java/frc/robot/subsystems/SSM.java
@@ -9,12 +9,17 @@ import frc.robot.commands.DriveCommands;
 public class SSM extends SubsystemBase {
     private final Arm m_arm;
     private final Elevator m_elevator;
-    public enum States {DISABLED, L1, L2, L3, L4, LOADINGSTATION, PROCESSOR, BARGE, GROUNDALGAE, ALGAELOW, ALGAEHIGH, Climb};
+
+    public enum States {
+        DISABLED, L1, L2, L3, L4, LOADINGSTATION, PROCESSOR, BARGE, GROUNDALGAE, ALGAELOW, ALGAEHIGH, Climb
+    };
+
     private boolean m_elevatorPauseHigh, m_elevatorPauseLow, m_armPauseHigh, m_armPauseLow;
     private States m_setpoint, m_queuedSetpoint;
-    private double m_as, m_es;
+    private double m_armSetpoint, m_elevatorSetpoint;
 
-    // First constructor accepts a constant as an initial setpoint and starts moving there immediately via periodic
+    // First constructor accepts a constant as an initial setpoint and starts moving
+    // there immediately via periodic
     public SSM(Arm arm, Elevator elevator, States setpoint) {
         m_arm = arm;
         m_elevator = elevator;
@@ -32,41 +37,54 @@ public class SSM extends SubsystemBase {
         SmartDashboard.putBoolean("m_elevatorPauseLow", m_elevatorPauseLow);
     }
 
-    // Second constructor defaults to disabled (do nothing until SetState is first called)
+    // Second constructor defaults to disabled (do nothing until SetState is first
+    // called)
     public SSM(Arm arm, Elevator elevator) {
         this(arm, elevator, States.DISABLED);
     }
 
-    // ALways start the elevator if elevator commanded up, only move to the danger point if the final setpoint is beyond.
-    // Always start the arm if the elevator commanded down, only move to the danger point if the final setpoint is beyond.
-    // kElevatorHighDanger - elevator must be higher than this to safely move arm above kArmHighDanger
-    // KElevatorLowDanter - elevator must be higher than this to safely move arm below kArmLowDanger
+    // ALways start the elevator if elevator commanded up, only move to the danger
+    // point if the final setpoint is beyond.
+    // Always start the arm if the elevator commanded down, only move to the danger
+    // point if the final setpoint is beyond.
+    // kElevatorHighDanger - elevator must be higher than this to safely move arm
+    // above kArmHighDanger
+    // KElevatorLowDanter - elevator must be higher than this to safely move arm
+    // below kArmLowDanger
     private void newState(States state) {
         int m_slewMode = 0;
 
         m_setpoint = state;
         SmartDashboard.putString("m_setpoint", m_setpoint.toString());
 
-        if (m_setpoint == States.DISABLED) return;
+        if (m_setpoint == States.DISABLED)
+            return;
 
         // Adjust the slew rate based on the setpoint
         switch (m_setpoint) {
             case L4:
+                if (m_slewMode != 1)
+                    DriveCommands.updateSlew(3.4, 3.4, 30.0);
+                m_slewMode = 1;
+                break;
             case BARGE:
-                if (m_slewMode != 1) DriveCommands.updateSlew(3.4, 3.4, 30.0);
-                    m_slewMode = 1;
-            break;
+                if (m_slewMode != 1)
+                    DriveCommands.updateSlew(3.4, 3.4, 30.0);
+                m_slewMode = 1;
+                break;
             case L3:
-                if (m_slewMode != 1) DriveCommands.updateSlew(5, 5, 30.0);
-                    m_slewMode = 1;
+                if (m_slewMode != 1)
+                    DriveCommands.updateSlew(5, 5, 30.0);
+                m_slewMode = 1;
             default:
-                if (m_slewMode !=2) DriveCommands.updateSlew(10.0, 10.0, 30.0);
+                if (m_slewMode != 2)
+                    DriveCommands.updateSlew(10.0, 10.0, 30.0);
                 m_slewMode = 2;
-            break;
+                break;
         }
 
-        m_as = getScoringArmPosition(m_setpoint);               // Grab some local variables for mulit reuse efficiency
-        m_es = getScoringElevatorPosition(m_setpoint);
+        m_armSetpoint = getScoringArmPosition(m_setpoint); // Grab some local variables for mulit reuse efficiency
+        m_elevatorSetpoint = getScoringElevatorPosition(m_setpoint);
 
         // Clear booleans
         m_armPauseHigh = false;
@@ -74,46 +92,54 @@ public class SSM extends SubsystemBase {
         m_elevatorPauseHigh = false;
         m_elevatorPauseLow = false;
 
-        if (m_elevator.getPosition() < m_es) {                      // If elevator going up...
-            m_elevator.setPosition(m_es);                           // Going up, always start elevator to setpoint
-            if ((m_as < ArmConstants.kArmHighDanger) && (m_elevator.getPosition() < ElevatorConstants.kElevatorHighDanger)) {
-                m_arm.setPosition(ArmConstants.kArmHighDanger);     // Pause arm at high danger before moving farther up
+        if (m_elevator.getPosition() < m_elevatorSetpoint) { // If elevator going up...
+            m_elevator.setPosition(m_elevatorSetpoint); // Going up, always start elevator to setpoint
+            if ((m_armSetpoint < ArmConstants.kArmHighDanger)
+                    && (m_elevator.getPosition() < ElevatorConstants.kElevatorHighDanger)) {
+                m_arm.setPosition(ArmConstants.kArmHighDanger); // Pause arm at high danger before moving farther up
                 m_armPauseHigh = true;
-            } else if ((m_as > ArmConstants.kArmLowDanger) && (m_elevator.getPosition() < ElevatorConstants.kElevatorLowDanger)) {
-                m_arm.setPosition(ArmConstants.kArmLowDanger);      // Pause arm at low danger before moving farther down
+            } else if ((m_armSetpoint > ArmConstants.kArmLowDanger)
+                    && (m_elevator.getPosition() < ElevatorConstants.kElevatorLowDanger)) {
+                m_arm.setPosition(ArmConstants.kArmLowDanger); // Pause arm at low danger before moving farther down
                 m_armPauseLow = true;
-            } else m_arm.setPosition(m_as);                         // No constraints, move arm to final setpoint
+            } else
+                m_arm.setPosition(m_armSetpoint); // No constraints, move arm to final setpoint
         } else {
-            m_arm.setPosition(m_as);                                // Going down, always start arm to setpoint
-            if ((m_es < ElevatorConstants.kElevatorHighDanger) && (m_arm.getPosition() < ArmConstants.kArmHighDanger)) {
-                m_elevator.setPosition(ElevatorConstants.kElevatorHighDanger);     // Pause elevator at high danger before moving farther down
+            m_arm.setPosition(m_armSetpoint); // Going down, always start arm to setpoint
+            if ((m_elevatorSetpoint < ElevatorConstants.kElevatorHighDanger) && (m_arm.getPosition() < ArmConstants.kArmHighDanger)) {
+                m_elevator.setPosition(ElevatorConstants.kElevatorHighDanger); // Pause elevator at high danger before
+                                                                               // moving farther down
                 m_elevatorPauseHigh = true;
-            } else if ((m_es < ElevatorConstants.kElevatorLowDanger) && (m_arm.getPosition() > ArmConstants.kArmLowDanger)) {
-                m_elevator.setPosition(ElevatorConstants.kElevatorLowDanger);      // Pause elevator at low danger before moving farther down
+            } else if ((m_elevatorSetpoint < ElevatorConstants.kElevatorLowDanger)
+                    && (m_arm.getPosition() > ArmConstants.kArmLowDanger)) {
+                m_elevator.setPosition(ElevatorConstants.kElevatorLowDanger); // Pause elevator at low danger before
+                                                                              // moving farther down
                 m_elevatorPauseLow = true;
             } else {
-                m_elevator.setPosition(m_es);                        // No constraints, move elevator to final setpoint
+                m_elevator.setPosition(m_elevatorSetpoint); // No constraints, move elevator to final setpoint
             }
         }
     }
 
+    public void periodic() {
 
-     public void periodic() {
+        if (m_setpoint != m_queuedSetpoint)
+            newState(m_queuedSetpoint); // Check for new state commanded
+        if (m_setpoint == States.DISABLED)
+            return;
 
-        if (m_setpoint != m_queuedSetpoint) newState(m_queuedSetpoint);  // Check for new state commanded
-        if (m_setpoint == States.DISABLED) return;
-
-        if (m_armPauseHigh && (m_elevator.getPosition() > ElevatorConstants.kElevatorHighDanger)) {           // Elevator going up
-            m_arm.setPosition(m_as);                     // Cleared, continue to final setpoint
+        if (m_armPauseHigh && (m_elevator.getPosition() > ElevatorConstants.kElevatorHighDanger)) { // Elevator going up
+            m_arm.setPosition(m_armSetpoint); // Cleared, continue to final setpoint
             m_armPauseHigh = false;
-        } else if (m_armPauseLow && (m_elevator.getPosition() > ElevatorConstants.kElevatorLowDanger)) {      // Elevator going up
-            m_arm.setPosition(m_as);                     // Cleared, continue to final setpoint
+        } else if (m_armPauseLow && (m_elevator.getPosition() > ElevatorConstants.kElevatorLowDanger)) { // Elevator
+                                                                                                         // going up
+            m_arm.setPosition(m_armSetpoint); // Cleared, continue to final setpoint
             m_armPauseLow = false;
-        } else if (m_elevatorPauseHigh && (m_arm.getPosition() > ArmConstants.kArmHighDanger)) {              // Elevator going down
-            m_elevator.setPosition(m_es);                // Cleared, continue to final setpoint
+        } else if (m_elevatorPauseHigh && (m_arm.getPosition() > ArmConstants.kArmHighDanger)) { // Elevator going down
+            m_elevator.setPosition(m_elevatorSetpoint); // Cleared, continue to final setpoint
             m_elevatorPauseHigh = false;
-        } else if (m_elevatorPauseLow && (m_arm.getPosition() < ArmConstants.kArmLowDanger)) {                // Elevator going down
-            m_elevator.setPosition(m_es);                // Cleared, continue to final setpoint
+        } else if (m_elevatorPauseLow && (m_arm.getPosition() < ArmConstants.kArmLowDanger)) { // Elevator going down
+            m_elevator.setPosition(m_elevatorSetpoint); // Cleared, continue to final setpoint
             m_elevatorPauseLow = false;
         }
         SmartDashboard.putBoolean("m_armPauseHigh", m_armPauseHigh);
@@ -138,7 +164,7 @@ public class SSM extends SubsystemBase {
             case BARGE -> ElevatorConstants.kElevatorBarge;
             case GROUNDALGAE -> ElevatorConstants.kElelvatorGroundAlgae;
             case ALGAEHIGH -> ElevatorConstants.kHighAlgeaGrab;
-            case ALGAELOW -> ElevatorConstants.kLowAlgeaGrab;   
+            case ALGAELOW -> ElevatorConstants.kLowAlgeaGrab;
             default -> 0.0;
         };
     }
@@ -159,13 +185,3 @@ public class SSM extends SubsystemBase {
         };
     }
 }
-
-
-
-
-
-
-
-
-
-
