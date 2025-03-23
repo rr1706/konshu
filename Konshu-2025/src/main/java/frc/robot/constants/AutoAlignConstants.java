2c9b0fd7ee8c9e1b95344f059bc806a466b78b7f
samdare2000 <sam.dare@thethriftybot.com>
2025-03-23 11:52:24 -0500

    Fixed all Reef Locations

--- Konshu-2025/src/main/java/frc/robot/constants/AutoAlignConstants.java
+++ Konshu-2025/src/main/java/frc/robot/constants/AutoAlignConstants.java
@@ -8,18 +8,19 @@ public final class AutoAlignConstants {
 
     public static final class RedAllianceConstants {
         public static final Rotation2d BLUE_ALLIANCE_ROTATION = Rotation2d.kZero;
-        public static final Translation2d kCL = new Translation2d(12.499, 3.433);
-        public static final Translation2d kCR = new Translation2d(12.783, 3.268);//.plus(new Translation2d(0.03464,-0.020)); 
-        public static final Translation2d kBL = new Translation2d(13.278, 3.269);//.plus(new Translation2d(0.03464,0.020));
-        public static final Translation2d kBR = new Translation2d(13.563, 3.432);//.plus(new Translation2d(0.03464,0.020)); 
-        public static final Translation2d kAL = new Translation2d(13.810, 3.862);
-        public static final Translation2d kAR = new Translation2d(13.811, 4.19);
-        public static final Translation2d kFL = new Translation2d(13.563, 4.619);
-        public static final Translation2d kFR = new Translation2d(13.279, 4.784);
-        public static final Translation2d kEL = new Translation2d(12.811, 4.783);
-        public static final Translation2d kER = new Translation2d(12.526, 4.62);
-        public static final Translation2d kDL = new Translation2d(12.279, 4.19);
-        public static final Translation2d kDR = new Translation2d(12.278, 3.862);
+        public static final Translation2d kAL = new Translation2d(13.8385195,3.861569);//(13.810, 3.862);
+        public static final Translation2d kAR = new Translation2d(13.8385195,4.190186);//(13.811, 4.19);
+        public static final Translation2d kBL = new Translation2d(13.3064215,3.268563);//(13.278, 3.269);
+        public static final Translation2d kBR = new Translation2d(13.5915835,3.431882);//(13.563, 3.432);
+        public static final Translation2d kCL = new Translation2d(12.5268135,3.431882);//(12.499, 3.433);
+        public static final Translation2d kCR = new Translation2d(12.8108335,3.268563);//(12.783, 3.268);
+        public static final Translation2d kDL = new Translation2d(12.2781625,4.190186);//(12.279, 4.19);
+        public static final Translation2d kDR = new Translation2d(12.2781625,3.861569);//(12.278, 3.862);
+        public static final Translation2d kEL = new Translation2d(12.8108335,4.784181);//(12.811, 4.783);
+        public static final Translation2d kER = new Translation2d(12.5268135,4.618883);//(12.526, 4.62);
+        public static final Translation2d kFL = new Translation2d(13.5915835,4.618883);//(13.563, 4.619);
+        public static final Translation2d kFR = new Translation2d(13.3064215,4.784181);//(13.279, 4.784);
+
         public static final Rotation2d kDAlgea = new Rotation2d(0);
         public static final Rotation2d kCAlgea = new Rotation2d(Math.PI / 3);
         public static final Rotation2d kBAlgea = new Rotation2d((2 * Math.PI) / 3);
@@ -29,24 +30,25 @@ public final class AutoAlignConstants {
     }
 
     public static final class BlueAllianceConstants {
-        public static final Translation2d kCL = new Translation2d(5.021, 4.619);
-        public static final Translation2d kCR = new Translation2d(4.737, 4.784);
-        public static final Translation2d kBL = new Translation2d(4.242, 4.783);
-        public static final Translation2d kBR = new Translation2d(3.957, 4.62);
-        public static final Translation2d kAL = new Translation2d(3.71, 4.19);
-        public static final Translation2d kAR = new Translation2d(3.709, 3.862);
-        public static final Translation2d kFL = new Translation2d(3.957, 3.433);
-        public static final Translation2d kFR = new Translation2d(4.241, 3.268);
-        public static final Translation2d kEL = new Translation2d(4.737, 3.269);
-        public static final Translation2d kER = new Translation2d(5.022, 3.432);
-        public static final Translation2d kDL = new Translation2d(5.269, 3.862);
-        public static final Translation2d kDR = new Translation2d(5.27, 4.19);
-        public static final Rotation2d kDAlgea = new Rotation2d(0+Math.PI);
-        public static final Rotation2d kCAlgea = new Rotation2d(Math.PI / 3+Math.PI);
-        public static final Rotation2d kBAlgea = new Rotation2d((2 * Math.PI) / 3+Math.PI);
-        public static final Rotation2d kAAlgea = new Rotation2d(Math.PI+Math.PI);
-        public static final Rotation2d kFAlgea = new Rotation2d((4 * Math.PI) / 3+Math.PI);
-        public static final Rotation2d kEAlgea = new Rotation2d((5 * Math.PI) / 3+Math.PI);
+        public static final Translation2d kAL = new Translation2d(3.7085865,4.190186);//(3.71, 4.19);
+        public static final Translation2d kAR = new Translation2d(3.7085865,3.861569);//(3.709, 3.862);
+        public static final Translation2d kBL = new Translation2d(4.2412575,4.784181);//(4.242, 4.783);
+        public static final Translation2d kBR = new Translation2d(3.9572385,4.618883);//(3.957, 4.62);
+        public static final Translation2d kCL = new Translation2d(5.0220075,4.618883);//(5.021, 4.619);
+        public static final Translation2d kCR = new Translation2d(4.7368455,4.784181);//(4.737, 4.784);
+        public static final Translation2d kDL = new Translation2d(5.2689445,3.861569);//(5.269, 3.862);
+        public static final Translation2d kDR = new Translation2d(5.2689445,4.190186);//(5.27, 4.19);
+        public static final Translation2d kEL = new Translation2d(4.7368455,3.268563);//(4.737, 3.269);
+        public static final Translation2d kER = new Translation2d(5.0220075,3.431882);//(5.022, 3.432);
+        public static final Translation2d kFL = new Translation2d(3.9572385,3.431882);//(3.957, 3.433);
+        public static final Translation2d kFR = new Translation2d(4.2412575,3.268563);//(4.241, 3.268);
+
+        public static final Rotation2d kDAlgea = new Rotation2d(0 + Math.PI);
+        public static final Rotation2d kCAlgea = new Rotation2d(Math.PI / 3 + Math.PI);
+        public static final Rotation2d kBAlgea = new Rotation2d((2 * Math.PI) / 3 + Math.PI);
+        public static final Rotation2d kAAlgea = new Rotation2d(Math.PI + Math.PI);
+        public static final Rotation2d kFAlgea = new Rotation2d((4 * Math.PI) / 3 + Math.PI);
+        public static final Rotation2d kEAlgea = new Rotation2d((5 * Math.PI) / 3 + Math.PI);
     }
 
     public static InterpolatingDoubleTreeMap ElevatorAutoAlignL1 = new InterpolatingDoubleTreeMap();
