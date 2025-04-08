2ef420cf652b153daf67e1a647135c330136bd6a
Izzy-hufty <izzyhufty@gmail.com>
2025-04-07 20:35:17 -0500

    L4 constant

--- Konshu-2025/src/main/java/frc/robot/constants/ElevatorConstants.java
+++ Konshu-2025/src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -27,7 +27,7 @@ public class ElevatorConstants {
     public static final double kElevatorL1 = 8.6;       // These are in inches
     public static final double kElevatorL2 = 22.0; 
     public static final double kElevatorL3 = 37.5; 
-    public static final double kElevatorL4 = 71.1; 
+    public static final double kElevatorL4 = 71.5; 
     public static final double kElevatorLoadingStation = 0.2; 
     public static final double kElevatorProcessor = 5.7;
     public static final double kElevatorBarge = 69.61; 
@@ -41,7 +41,7 @@ public class ElevatorConstants {
     public static final double kLowAlgeaGrab = 19.5;
     public static final double kHighAlgeaGrab = kLowAlgeaGrab+15.75;
 
-    public static final double kUpperLimitElevator = 72.7;    // Inches
+    public static final double kUpperLimitElevator = 73.5;    // Inches
     public static final double kULowerLimitElevator = 0;
 
     public static final double kPulleyTeeth = 36.0;
